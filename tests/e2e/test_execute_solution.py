3b21a9f93aa7e9303b8e981c186aceb88d99fdc3
Ido Shapira <ido.github.mail@gmail.com>
2025-06-03 19:31:57 +0800

    flake8

--- tests/e2e/test_execute_solution.py
+++ tests/e2e/test_execute_solution.py
@@ -1,13 +1,18 @@
 import requests
 
+
 def test_health_check():
     resp = requests.get("http://localhost:8080/health")
     assert resp.status_code == 200
 
+
 def test_python_execution():
-    resp = requests.post("http://localhost:8080/execute_solution", json={
-    "question_id": 1,
-    "code": "ZGVmIGFkZChzZWxmLCBhLCBiKToNCiAgICByZXR1cm4gYSArIGI=",
-    "language": "python"
-})
+    resp = requests.post(
+        "http://localhost:8080/execute_solution",
+        json={
+            "question_id": 1,
+            "code": "ZGVmIGFkZChzZWxmLCBhLCBiKToNCiAgICByZXR1cm4gYSArIGI=",
+            "language": "python",
+        },
+    )
     assert resp.json()["test_result"] is True
