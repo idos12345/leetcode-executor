e4c415aa42d79e1cdd5726ab7576628f35a362ed
Ido Shapira <ido.github.mail@gmail.com>
2025-06-03 18:43:08 +0800

    e2e

--- leetcode_execution_api/bl/k8s_handler/k8s_job_executor.py
+++ leetcode_execution_api/bl/k8s_handler/k8s_job_executor.py
@@ -1,7 +1,7 @@
 import yaml
 from kubernetes import client, config
 from leetcode_execution_api.core.config import settings
-from leetcode_execution_api.core import constans
+from leetcode_execution_api import constants
 
 
 class K8sJobExecutor:
@@ -11,7 +11,7 @@ class K8sJobExecutor:
 
     @staticmethod
     def execute_job(
-        image_name: str, yaml_path=constans.K8S_JOB_YAML_PATH, namespace="default"
+        image_name: str, yaml_path=constants.K8S_JOB_YAML_PATH, namespace="default"
     ) -> None:
         """
         Create and execute k8s job for image
