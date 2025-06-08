5ae126f7d6a1b3f6184891171debfad7e2a77d94
Ido Shapira <idoshapira2@mail.tau.ac.il>
2025-06-08 14:44:19 +0300

    prod

--- leetcode_execution_api/core/config.py
+++ leetcode_execution_api/core/config.py
@@ -7,6 +7,9 @@ load_dotenv()
 class Settings(BaseSettings):
     DATABASE_URL: str
     REGISTRY_URL: str
+    NAMESPACE: str
+    SWR_LOGIN_U: str
+    SWR_LOGIN_P: str
 
     class Config:
         env_file = ".env"
