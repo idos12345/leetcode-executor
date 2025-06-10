5fd6026586ae1b81c0323187f110bc34356db3b4
Ido Shapira <80099123+idos12345@users.noreply.github.com>
2025-06-10 16:35:30 +0300

    cicd+cli
    * cicd
    
    * cli

--- cli.py
+++ cli.py
@@ -1,13 +1,9 @@
 import argparse
 import base64
-import itertools
-import sys
-import threading
-import time
 
 import requests
 
-API_URL = "http://127.0.0.1:8080/execute_solution/"
+DEFAULT_API_URL = "http://213.250.144.112:30090/execute_solution/"
 
 
 def encode_file_to_base64(file_path):
@@ -15,7 +11,8 @@ def encode_file_to_base64(file_path):
     with open(file_path, "rb") as file:
         return base64.b64encode(file.read()).decode("utf-8")
 
-def execute_solution(question_id, file_path, language):
+
+def execute_solution(question_id, file_path, language, api_url=DEFAULT_API_URL):
     """Sends a POST request with the encoded code to the API."""
     encoded_code = encode_file_to_base64(file_path)
 
@@ -26,7 +23,7 @@ def execute_solution(question_id, file_path, language):
     }
 
     print("\n⏳ Processing...\n")
-    response = requests.post(API_URL, json=payload)
+    response = requests.post(api_url, json=payload)
 
     if response.status_code == 200:
         result = response.json()
@@ -47,9 +44,11 @@ def main():
     parser.add_argument("question_id", type=int, help="Question ID")
     parser.add_argument("file", type=str, help="Path to the code file")
     parser.add_argument("language", type=str, help="Programming language")
+    parser.add_argument("--api_url", type=str, help="API URL")
 
     args = parser.parse_args()
-    execute_solution(args.question_id, args.file, args.language)
+    api_url = args.api_url if args.api_url else DEFAULT_API_URL
+    execute_solution(args.question_id, args.file, args.language, api_url=api_url)
 
 
 if __name__ == "__main__":
