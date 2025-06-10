63465fc919eb4eb883d0bb2886902d4ab15eb85f
Ido Shapira <ido.github.mail@gmail.com>
2025-06-10 18:29:31 +0800

    cicd

--- leetcode_execution_api/core/config.py
+++ leetcode_execution_api/core/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+from typing import Optional
 
 load_dotenv()
 
@@ -8,8 +9,8 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     REGISTRY_URL: str
     NAMESPACE: str
-    SWR_LOGIN_U: str
-    SWR_LOGIN_P: str
+    SWR_LOGIN_U: Optional[str]
+    SWR_LOGIN_P: Optional[str]
 
     class Config:
         env_file = ".env"
